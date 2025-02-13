f36661a8f5abf86ecc44049baa059bc3700c8567
KaytmazV <vkaytmaz@gmail.com>
2025-02-13 19:00:14 +0300

    v. 0.0.1

--- src/main/java/com/volkankaytmaz/detectivegame/model/Question.java
+++ src/main/java/com/volkankaytmaz/detectivegame/model/Question.java
@@ -1,4 +1,19 @@
 package com.volkankaytmaz.detectivegame.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Basic
+    @Column(columnDefinition = "TEXT")
+    private String question_text;
+
+    private int questionPersonId;
 }
